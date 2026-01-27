5b206f4a7e58ae58053a759947bed3d29458bd17
Vishrutha1909 <vishruthar19@gmail.com>
2026-01-27 15:20:11 -0500

    .

--- backend/src/main/java/com/wecureit/repository/PatientRepository.java
+++ backend/src/main/java/com/wecureit/repository/PatientRepository.java
@@ -1,10 +1,11 @@
 package com.wecureit.repository;
 
-import com.wecureit.entity.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wecureit.entity.Patient;
 
 public interface PatientRepository extends JpaRepository<Patient, UUID> {
 
