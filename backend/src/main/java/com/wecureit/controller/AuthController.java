69a9243a77f7a37fc80533b7f939c7c2d834b2bd
Vishrutha1909 <vishruthar19@gmail.com>
2026-02-09 21:57:57 -0500

    .

--- backend/src/main/java/com/wecureit/controller/AuthController.java
+++ backend/src/main/java/com/wecureit/controller/AuthController.java
@@ -4,8 +4,8 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
