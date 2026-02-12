6d6e8a56011e1528aaebee19437e59ea55944296
ullasbc <ullasbc02@gmail.com>
2026-02-12 12:26:40 -0500

    Deduplication of the availability by doctors

--- backend/src/main/java/com/wecureit/controller/patient/PatientBookingController.java
+++ backend/src/main/java/com/wecureit/controller/patient/PatientBookingController.java
@@ -4,10 +4,8 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import com.wecureit.dto.response.BookingDropdownResponse;
 import com.wecureit.service.PatientBookingService;
